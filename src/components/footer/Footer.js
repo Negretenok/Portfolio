2968d33eecf9a6ce8f4f392bee29864ec78d3f3a
NurKazak <nurjigitnurlanbekovich2007@gmail.com>
2024-11-23 14:33:27 +0600

    fix contacts

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -23,8 +23,8 @@ const Footer = () => {
                     </div>
                     <div className='contacts'>
                         <a href='https://www.instagram.com/k4zak_11/profilecard/?igsh=dTA4MmlvemR0Mms3'><img className='contact' src={instagram} alt='inst'/></a>
-                        <a href='https://wa.me/qr/J5OC7SRORR2YA1'><img className='contact' src={tiktok} alt='tt'/></a>
-                        <a href='https://www.tiktok.com/@nnurzhigit_11?_t=8raWEPNWCLY&_r=1'><img className='contact' src={whatsapp} alt='waths'/></a>
+                        <a href='https://wa.me/qr/J5OC7SRORR2YA1'><img className='contact' src={whatsapp} alt='waths'/></a>
+                        <a href='https://www.tiktok.com/@nnurzhigit_11?_t=8raWEPNWCLY&_r=1'><img className='contact' src={tiktok} alt='tt'/></a>
                         <a href='http://t.me/NNurzh1git'><img className='contact' src={telegram}alt='tg'/></a>
                     </div>
                 </div>
